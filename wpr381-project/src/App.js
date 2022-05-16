24a0e86e5262217b92dc237b3f5c3e3dd0fdb851
Raymond <576839@student.belgiumcampus.ac.za>
2022-05-16 21:42:41 +0200

    basic file and app structure

--- wpr381-project/src/App.js
+++ wpr381-project/src/App.js
@@ -2,9 +2,35 @@ import React, {useState} from 'react';
 import axios from 'axios'
 
 function App() {
+
+  //const url = `https://api.openweathermap.org/data/2.5/weather?zip=0182,ZA&appid=8a591b7d222aae763fc9d6e5a1f06b16`
+
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Pretoria</p>
+          </div>
+          <div className="temp">
+            <h1>65 deg F</h1>
+          </div>
+          <div className="description">
+            <p>clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65 degrees</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            <p>12 MPH</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
